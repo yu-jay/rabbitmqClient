23447ae927c5c173352e5fd1d1e6db82013e84c0
余杰 <yujie@ahsoft.com>
2019-03-14 10:10:26 +0800

    初始化项目

---
+++ src/main/java/com/github/yu_jay/rabbitmqClient/App.java
@@ -0,0 +1,13 @@
+package com.github.yu_jay.rabbitmqClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
